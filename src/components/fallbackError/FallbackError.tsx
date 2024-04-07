33f686336c99111738ac645c656f5513f1fee6dc
Jonika <katyadyduch@gmail.com>
2024-04-07 17:35:46 +0300

    ErrorBoundary

---
+++ src/components/fallbackError/FallbackError.tsx
@@ -0,0 +1,7 @@
+export const FallbackError = () => {
+  return (
+    <>
+      <h1>The cat made a mistake</h1>
+    </>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -5,20 +5,24 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { fetcher } from './api/fetcher.ts';
 import { SWRConfig } from 'swr';
+import { ErrorBoundary } from 'react-error-boundary';
+import { FallbackError } from './components/fallbackError/FallbackError.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <React.Suspense fallback={<>...</>}>
-        <SWRConfig
-          value={{
-            fetcher,
-            suspense: true
-          }}
-        >
-          <App />
-        </SWRConfig>
-      </React.Suspense>
-    </BrowserRouter>
+    <ErrorBoundary FallbackComponent={FallbackError}>
+      <BrowserRouter>
+        <React.Suspense fallback={<>...</>}>
+          <SWRConfig
+            value={{
+              fetcher,
+              suspense: true
+            }}
+          >
+            <App />
+          </SWRConfig>
+        </React.Suspense>
+      </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>
 );
